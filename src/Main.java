c3a33eafbc90502e6eaf33dd3c1726bade112a90
DemchaAV <demchaav@gmail.com>
2024-04-02 20:40:24 +0100

    additional

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,13 @@
-import org.jsoup.parser.HtmlTreeBuilder;
-import org.jsoup.parser.Parser;
-
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         long startTime = System.currentTimeMillis();
         String url = "https://www.microsoft.com/";
         String outPath = "C:/Users/Demch/OneDrive/Рабочий стол";
         SvgImporter svg = new SvgImporter(url, outPath);
-        try {
-            svg.read();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        svg.read();
         svg.write();
         double spentTime = (double) (System.currentTimeMillis() - startTime) / 1000;
         String second = spentTime > 2 ? " seconds" : " second";
