0f0a93a8d1693ae98ed99f2e02437220a007c487
DemchaAV <demchaav@gmail.com>
2024-04-10 11:28:44 +0100

    Version control

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         long startTime = System.currentTimeMillis();
-        String url = "https://www.microsoft.com/";
+        String url = "https://developers.google.com/";
         String outPath = "C:/Users/Demch/OneDrive/Рабочий стол";
         SvgImporter svg = new SvgImporter(url, outPath);
         svg.read();
